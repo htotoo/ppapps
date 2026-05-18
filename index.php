1deae7d941c9f9f56027d187be98f2bbbc14817d
HTotoo <ttotoo@gmail.com>
2026-05-18 18:55:15 +0200

    More apps

--- index.php
+++ index.php
@@ -134,6 +134,14 @@ if ($selectedCategory !== '') {
                         Splash screens
                         <svg class="w-4 h-4 opacity-50 group-hover:opacity-100" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
                     </a>
+                    <a href="https://github.com/htotoo/ppapps" target="_blank" class="px-4 py-2 rounded-md transition-colors duration-200 hover:bg-gray-800 text-gray-400 hover:text-white flex items-center justify-between group">
+                        Web Source
+                    <svg class="w-4 h-4 opacity-50 group-hover:opacity-100" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
+                    </a>
+                    <a href="https://github.com/htotoo/mayhem-mdk" target="_blank" class="px-4 py-2 rounded-md transition-colors duration-200 hover:bg-gray-800 text-gray-400 hover:text-white flex items-center justify-between group">
+                        Apps Source
+                    <svg class="w-4 h-4 opacity-50 group-hover:opacity-100" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
+                    </a>
                 </nav>
             </aside>
 
